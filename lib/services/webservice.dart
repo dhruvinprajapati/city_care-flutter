6c8b74f66fe66ffc41bcd555e2e597dc948eb2a5
dhrvuin <djprajapati1056@outlook.com>
2020-03-23 15:42:58 +0530

    Dio work fine

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -4,24 +4,29 @@ import 'package:care_city/models/incident.dart';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
-class WebService{
-
-  Future<void> saveIncident()async{
-     var dio = new Dio();
-    final url ="http://test.karbh.com/test";
-    await dio.post(
+class WebService {
+  Future<void> saveIncident() async {
+    var dio = new Dio();
+    final url = "http://test.karbh.com/test";
+    FormData form=FormData.fromMap({"title": "Dhruvin", "description": "prajapati"});
+    try{
+    Response response = await dio.post(
       url,
-      data: {"title":"Dhruvin","description":"prajapati"},
-      options: Options(contentType:"application/x-www-from-urlencoded"),
-      );
-  //   await http.post(
-  //     url,
-  //     body: {"title":"Dhruvin","description":"prajapati"},
-  //     headers: {
-  //   'Content-type' : 'application/x-www-form-urlencoded'
-  // },
-  //     );
-
+      data: form,
+      options: Options(
+          contentType: "application/x-www-from-urlencoded"),
+    );
+    print(response);
+    }
+    catch(e){
+      print(e);
+    }
+    //   await http.post(
+    //     url,
+    //     body: {"title":"Dhruvin","description":"prajapati"},
+    //     headers: {
+    //   'Content-type' : 'application/x-www-form-urlencoded'
+    // },
+    //     );
   }
-
 }
