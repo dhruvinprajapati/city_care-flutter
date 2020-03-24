f36fdde1570a5942637cde1e9150c9d895c7335d
dhrvuin <djprajapati1056@outlook.com>
2020-03-24 11:08:28 +0530

    create take picture buttin and set display item in that

--- lib/pages/incident_report_page.dart
+++ lib/pages/incident_report_page.dart
@@ -1,12 +1,31 @@
+
+
 import 'package:care_city/view_models/report_incident_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class IncidentReportPage extends StatelessWidget {
-  
+
+  void _showPhotoSelectionOption(BuildContext context){
+    showModalBottomSheet(context: context, builder: (context){
+      return Container(
+        height: 150,
+        child: Column(children:<Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_camera),
+            title: Text("take a picture"),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text("Select from photo library"),
+          )
+        ]),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     final vm = Provider.of<ReportIncidentViewModel>(context);
 
     return Scaffold(
@@ -15,8 +34,16 @@ class IncidentReportPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
           child: Column(children: <Widget>[
+        Image.asset("images/1234.jpg"),
+        FlatButton(
+          child: Text("take Picture"),
+          color: Colors.blue,
+          onPressed: () {
+            _showPhotoSelectionOption(context);
+          },
+        ),
         TextField(
-          onChanged: (value)=>vm.title = value,
+          onChanged: (value) => vm.title = value,
           decoration: InputDecoration(
               labelText: "Enter title",
               border:
@@ -30,7 +57,7 @@ class IncidentReportPage extends StatelessWidget {
           decoration: InputDecoration(labelText: "Enter Description"),
         ),
         FlatButton(
-          onPressed: (){
+          onPressed: () {
             vm.saveIncident();
             Navigator.pop(context);
           },
